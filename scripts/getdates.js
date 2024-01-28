da764d1a458897a865fe87f4fd7a3f87b043b329
angelusa <boatkayili@gmail.com>
2024-01-28 00:40:02 -0800

    map

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,2 +1,11 @@
 var currentYear = new Date().getFullYear();
 document.getElementById('curretYear').innerText = currentYear;
+
+document.addEventListener('DOMContentLoaded', function () {
+    const hamburgerButton = document.querySelector('.hamburger');
+    const navMenu = document.querySelector('.nav-menu');
+
+    hamburgerButton.addEventListener('click', function () {
+        navMenu.classList.toggle('show-menu');
+    });
+});
