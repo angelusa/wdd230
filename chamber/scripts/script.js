fcf4ef33d5a2342013f9e522af621726ebc852d9
angelusa <boatkayili@gmail.com>
2024-03-26 11:22:15 -0700

    scirpt

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -25,27 +25,3 @@ const banner = document.querySelector(".banner");
 if (banner && currentDate.getDay() <= 2 && currentDate.getDay() > 0) {
     banner.style.display = "block";
 }
-
-document.addEventListener("DOMContentLoaded", function() {
-    var currentDate = new Date();
-    var currentDateMs = currentDate.getTime();
-    var lastVisitDateMs = localStorage.getItem("lastVisitDate");
-  
-    if (!lastVisitDateMs) {
-        document.getElementById("sidebar").innerText = "Welcome! Let us know if you have any questions.";
-    } else {
-        var differenceMs = currentDateMs - parseInt(lastVisitDateMs);
-        var oneDayMs = 1000 * 60 * 60 * 24;
-  
-        if (differenceMs < oneDayMs) {
-            document.getElementById("sidebar").innerText = "Back so soon! Awesome!";
-        } else {
-            var differenceDays = Math.floor(differenceMs / oneDayMs);
-            var message = (differenceDays === 1) ? "day" : "days";
-  
-            document.getElementById("sidebar").innerText = "You last visited " + differenceDays + " " + message + " ago.";
-        }
-    }
-  
-    localStorage.setItem("lastVisitDate", currentDateMs);
-});
