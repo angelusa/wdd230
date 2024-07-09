6fa7017c1043d72502bf5057ad472e143d4151ce
angelusa <boatkayili@gmail.com>
2024-07-09 16:12:26 -0700

    link join.html

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -1,8 +1,11 @@
-document.getElementById('myForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent the default form submission
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById('formLoadedTime').value = new Date().toISOString();
 
-    // You can add form validation or other logic here
+    document.getElementById('joinForm').addEventListener('submit', function(event) {
+        event.preventDefault(); // Prevent the default form submission
 
-    // Redirect to the thank you page
-    window.location.href = "https://angelusa.github.io/wdd230/chamber/thankyou.html";
+        // Redirect to the thank you page
+        window.location.href = "https://angelusa.github.io/wdd230/chamber/thankyou.html";
+    });
 });
+
