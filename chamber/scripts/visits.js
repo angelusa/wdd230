e6305e411d6155d16433b6df499b975941095ca6
angelusa <boatkayili@gmail.com>
2024-07-10 15:01:09 -0700

    update

---
+++ chamber/scripts/visits.js
@@ -0,0 +1,31 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    var visitMessage = document.getElementById('visit-message');
+    var lastVisit = localStorage.getItem('lastVisit');
+    var currentVisit = Date.now();
+    var currentDate = new Date(currentVisit).toLocaleDateString();
+
+    console.log('Current Visit:', currentVisit);
+    console.log('Last Visit:', lastVisit);
+
+    if (!lastVisit) {
+        visitMessage.textContent = 'Welcome! Let us know if you have any questions. Today\'s date is ' + currentDate;
+    } else {
+        var timeDiff = currentVisit - lastVisit;
+        var daysDiff = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+        var lastVisitDate = new Date(parseInt(lastVisit)).toLocaleDateString();
+
+        console.log('Time Difference:', timeDiff);
+        console.log('Days Difference:', daysDiff);
+
+        if (daysDiff < 1) {
+            visitMessage.textContent = 'Back so soon! Awesome!  ' + currentDate;
+        } else if (daysDiff === 1) {
+            visitMessage.textContent = 'You last visited 1 day ago on ' + lastVisitDate + '. Today\'s date is ' + currentDate;
+        } else {
+            visitMessage.textContent = 'You last visited ' + daysDiff + ' days ago on ' + lastVisitDate + '. Today\'s date is ' + currentDate;
+        }
+    }
+
+    localStorage.setItem('lastVisit', currentVisit);
+});
---
+++ scripts/getDates.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Set the current year in the footer
+    document.getElementById('year').textContent = new Date().getFullYear();
+    
+    // Set the last modified date in the footer
+    document.getElementById('lastModified').textContent = 'Last Modified: ' + document.lastModified;
+  });
+  
