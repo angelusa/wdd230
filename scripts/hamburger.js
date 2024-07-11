5110bd644852873a0257f6e66fe4e1402cfdb0b5
angelusa <boatkayili@gmail.com>
2024-07-11 00:06:23 -0700

    update hamburger menu

--- scripts/hamburger.js
+++ scripts/hamburger.js
@@ -1,15 +1,14 @@
-//  hamburger.js 
 document.addEventListener('DOMContentLoaded', function() {
-    const hamburger = document.getElementById('hamburger');
-    const navMenu = document.getElementById('nav-menu');
-  
-    hamburger.addEventListener('click', function() {
-      if (navMenu.classList.contains('show')) {
-        navMenu.classList.remove('show');
-        hamburger.innerHTML = '&#9776;'; // Hamburger icon
-      } else {
-        navMenu.classList.add('show');
-        hamburger.innerHTML = '&times;'; // Close icon
-      }
-    });
+  const hamburger = document.getElementById('hamburger');
+  const navMenu = document.getElementById('nav-menu');
+
+  hamburger.addEventListener('click', function() {
+    if (navMenu.classList.contains('show')) {
+      navMenu.classList.remove('show');
+      hamburger.innerHTML = '&#9776;'; // Hamburger icon
+    } else {
+      navMenu.classList.add('show');
+      hamburger.innerHTML = '&times;'; // Close icon
+    }
   });
+});
